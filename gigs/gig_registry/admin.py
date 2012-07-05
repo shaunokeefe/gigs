365ffd62b0159e71f1bd4274c2ac59691b52a6a4
Shaun O'Keefe <shaun.okeefe.0@gmail.com>
2012-07-05 15:27:45 +1000

    A few admin changes

--- gigs/gig_registry/admin.py
+++ gigs/gig_registry/admin.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 from gigs.gig_registry import models
 
 
+class VenueAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'uid':('name',)}
+
 class MusicianInline(admin.TabularInline):
     fields = ['musician', 'started', 'finished', 'date_of_birth', 'instrument',]
-    #formfield_overrides = {
-    #        django_models.DateField: {'widget': SelectDateWidget},
-    #        }
     model = models.Musician
 
 #class MusicianAdmin(admin.ModelAdmin):
@@ -29,24 +29,23 @@ class BandAdmin(admin.ModelAdmin):
     inlines = [MembershipInline]
 
 class BandInline(admin.TabularInline):
-
     model = models.Gig.bands.through
 
 
 class GigAdmin(admin.ModelAdmin):
     fieldsets = [
-            (None, {'fields': ['venue','bands']}),
+            (None, {'fields': ['name', 'venue','bands', 'cost']}),
             ('Dates', {'fields': ['start', 'finish']}),
+            ('Meta', {'fields': ['comment']}),
         ]
-
+    
     filter_horizontal = ('bands',)
-    #inlines = [BandInline]#, TestInline]# VenueInline]
     list_filter = ('venue', 'bands',)
 
 admin.site.register(models.Band, BandAdmin)
 admin.site.register(models.Musician)#, MusicianAdmin)
 admin.site.register(models.Owner)
-admin.site.register(models.Venue)
+admin.site.register(models.Venue, VenueAdmin)
 admin.site.register(models.Location)
 admin.site.register(models.Genre)
 admin.site.register(models.Gig, GigAdmin)
