73f066e1d0b877299bbe98ed312fc2d7a653db5e
Shaun O'Keefe <shaun.okeefe.0@gmail.com>
2012-08-01 19:21:38 +1000

    conditional display of maps depending on whether objects have locations associated with them

--- gigs/maps/templatetags/maps.py
+++ gigs/maps/templatetags/maps.py
@@ -10,6 +10,34 @@ from gigs.gig_registry.models import Location, Gig, Venue, Band
 class MapForm(forms.Form):
     map = forms.Field(widget=GoogleMap(attrs={'width':510, 'height':510}))
 
+    def has_locations(self):
+        return True
+
+def get_location_queryset(locatables):
+    if isinstance(locatables, QuerySet):
+        cls = locatables.model
+        if cls == Location:
+            return locatables
+    else:
+        try:
+            cls = locatables.__class__
+            if cls == Location:
+                # forgive me
+                return cls.objects.filter(pk=locatables.pk)
+        except AttributeError:
+            return Location.objects.none()
+     
+    funcs = {
+            Gig: Location.objects.for_gigs,
+            Venue: Location.objects.for_venues,
+            Band: Location.objects.for_bands,
+            }
+    
+    try:
+        func = funcs[cls]
+        return func(locatables)
+    except KeyError:
+        return Location.objects.none()
 
 class MapNode(template.Node):
     
@@ -32,31 +60,33 @@ class MapNode(template.Node):
         except template.VariableDoesNotExist:
             return ''
         
-        if isinstance(locatables, QuerySet):
-            cls = locatables.model
-        else:
-            try:
-                cls = locatables.__class__
-                if cls == Location:
-                    locatables = [locatables]
-            except AttributeError:
-                return ''
+        locations = get_location_queryset(locatables)
 
-        funcs = {
-                Gig: Location.objects.for_gigs,
-                Venue: Location.objects.for_venues,
-                Band: Location.objects.for_bands,
-                }
-        try:
-            func = funcs[cls]
-            locations = func(locatables)
-        except KeyError:
-            if cls == Location:
-                locations = locatables
-            else:
-                return ''
-
-
+ #       if isinstance(locatables, QuerySet):
+ #           cls = locatables.model
+ #       else:
+ #           try:
+ #               cls = locatables.__class__
+ #               if cls == Location:
+ #                   locatables = [locatables]
+ #           except AttributeError:
+ #               return ''
+ #       
+ #       funcs = {
+ #               Gig: Location.objects.for_gigs,
+ #               Venue: Location.objects.for_venues,
+ #               Band: Location.objects.for_bands,
+ #               }
+        
+#        try:
+#            func = funcs[cls]
+#            locations = func(locatables)
+#        except KeyError:
+#            if cls == Location:
+#                locations = locatables
+#            else:
+#                return ''
+#
         for location in locations:
             lat = location.lat
             lon = location.lon
@@ -75,22 +105,29 @@ class GigSearchMapNode(MapNode):
         except template.VariableDoesNotExist:
             return ''
         
+        gigs = [gig_search_result.object for gig_search_result in locatables] 
+        #for gig_search_result in locatables:
+            #gig = gig_search_result.object
+        locations = get_location_queryset(gigs)
+            #if gig.venue and  gig.venue.location:
+            #    lat = gig.venue.location.lat
+            #    lon = gig.venue.location.lon
+        for location in locations:
+            lat = location.lat
+            lon = location.lon
+
+            marker = maps.Marker(opts = {
+                'map': self.gmap,
+                'position': maps.LatLng(lat, lon),
+                })
+            info = maps.InfoWindow({
+                        'content': "gig location",
+                        'disableAutoPan': True
+                                    })
+            info.open(self.gmap, marker)
         
-        for gig_search_result in locatables:
-            gig = gig_search_result.object
-            if gig.venue and  gig.venue.location:
-                lat = gig.venue.location.lat
-                lon = gig.venue.location.lon
-                marker = maps.Marker(opts = {
-                    'map': self.gmap,
-                    'position': maps.LatLng(lat, lon),
-                    })
-                info = maps.InfoWindow({
-                            'content': "gig location",
-                            'disableAutoPan': True
-                                        })
-                info.open(self.gmap, marker)
-                context['form'] = MapForm(initial={'map':self.gmap})
+        context['form'] = MapForm(initial={'map':self.gmap})
+        
         return ''
 
 def do_map(parser, token):
@@ -104,6 +141,11 @@ def do_map(parser, token):
         return GigSearchMapNode(instances)
     return MapNode(instances)
 
+def has_locations(locatable):
+    return get_location_queryset(locatable).count() != 0
+
+
 register = template.Library()
+register.filter('has_locations', has_locations)
 register.tag('map', do_map)
 register.tag('search_map', do_map)
