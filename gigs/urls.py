f776ad3874e1b75f39bf03ad34ef1f9c0f1c689b
Shaun O'Keefe <shaun.okeefe.0@gmail.com>
2012-11-14 11:09:16 +1100

    urls for directly accessing downloads of gigs, venues and locations

--- gigs/urls.py
+++ gigs/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 from gigs.portal import urls as portal_urls
 from gigs.search import urls as search_urls
+from gigs.gig_registry.models import Gig, Venue, Location
 
 admin.autodiscover()
 
@@ -13,6 +14,15 @@ urlpatterns = patterns(
     (r'', include(portal_urls)),
     (r'^admin/', include(admin.site.urls)),
     (r'^search/', include(search_urls)),
+    (r'^export_gigs/$', 'django_tablib.views.export', {
+            'model': Gig,
+            }),
+    (r'^export_venues/$', 'django_tablib.views.export', {
+            'model': Venue,
+            }),
+    (r'^export_locations/$', 'django_tablib.views.export', {
+            'model': Location,
+            }),
 )
 
 
