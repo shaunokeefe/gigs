e6b9935a13e91ea57ba876b8f6389399a653d18d
Shaun O'Keefe <shaun.okeefe.0@gmail.com>
2012-12-13 09:56:19 +1100

    updated search index to support filtering

--- gigs/search/search_indexes.py
+++ gigs/search/search_indexes.py
@@ -1,9 +1,26 @@
 from gigs.gig_registry.models import Gig
-from haystack.indexes import RealTimeSearchIndex, CharField
+from haystack.indexes import RealTimeSearchIndex, \
+    CharField, DateField, FloatField, MultiValueField
 from haystack import site
 
 class GigIndex(RealTimeSearchIndex):
     text = CharField(document=True, use_template=True)
+    tmp = CharField()
+    start = DateField(model_attr='start')
+    venue_name = CharField(model_attr='venue__name')
+    bands = MultiValueField()
+    cost = FloatField(model_attr='cost', default=0)
+    name = CharField(model_attr='name', default=0)
+    venue_location = CharField()
+
+    def prepare_bands(self, obj):
+        return [band.name for band in obj.bands.all()]
+
+    def prepare_location(self, obj):
+        return str(obj.venue.location)
+    
+    def prepare_tmp(self, obj):
+        return 'tmp'
 
     def index_queryset(self):
         return Gig.objects.all()
