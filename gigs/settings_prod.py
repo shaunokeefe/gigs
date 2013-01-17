76891e76c342a752e2ede664b498dd56bdc6419e
Shaun O'Keefe <shaun.okeefe.0@gmail.com>
2013-01-18 01:38:42 +1100

    add default logging settings
    fixed settings

--- gigs/settings_prod.py
+++ gigs/settings_prod.py
@@ -8,3 +8,44 @@ ADMINS = ()
 INSTALLED_APPS += (
     'gunicorn',
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+         },
+    },
+    'handlers': {
+        'default': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': '/var/log/tugg/tugg.log',
+            'maxBytes': 1024*1024*50, # 50 MB
+            'backupCount': 5,
+            'formatter':'standard',
+            },
+        'request_handler': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': '/var/log/tugg/tugg_request.log',
+            'maxBytes': 1024*1024*50, # 5 MB
+            'backupCount': 5,
+            'formatter':'standard',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'django.request': {
+            'handlers': ['request_handler'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+    },
+}
+
