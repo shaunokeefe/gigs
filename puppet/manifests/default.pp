efa2cf7b4becfc0f91ad4c8929dde196c0097284
shaunokeefe <shaun.okeefe.0@gmail.com>
2013-08-15 22:43:27 +1000

    add Vagrant config and required puppet modules

---
+++ puppet/manifests/default.pp
@@ -0,0 +1,8 @@
+tugg { 'tugg-instance':
+  user_password=>'user_password',
+  mysqldb_name=> 'mysqldb_name',
+  mysql_server_root_password=>'mysql_server_root_password',
+  mysqldb_username=>'mysqldb_username',
+  mysqldb_password=>'mysqldb_password',
+  user_name=>'user_name',
+}
